00243dc4fd1c3ec467e97090f98cc9e591a9c2a5
faran25 <mohd.faran25@gmail.com>
2020-12-26 18:29:39 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 38.9 MB (100.00%)

---
+++ my-folder/0001-two-sum/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        
+        //int index=0;
+        //Arrays.sort(nums);
+        
+        
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=i+1;j<nums.length;j++)
+            {
+                if(nums[j]==target-nums[i])
+                {
+                    return new int[]{i,j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+        
+        
+        
+    }
+}
