75b8f5ef5b69a2001f91c96d94e687ce730d8206
faran25 <mohd.faran25@gmail.com>
2024-04-26 19:25:15 +0000

    [LeetCode Sync] Runtime - 1 ms (99.37%), Memory - 56.7 MB (87.77%)

--- my-folder/0053-maximum-subarray/solution.java
+++ my-folder/0053-maximum-subarray/solution.java
@@ -1,19 +1,17 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int max1=nums[0];
-        int max2=nums[0];
-        
-        for(int i=1;i<nums.length;i++)
+        int max=nums[0];
+        int sum=0;
+
+        for(int i=0;i<nums.length;i++)
         {
-            if(max1>0)
+            sum=sum+nums[i];
+            if(sum>max) max=sum;
+            if(sum<0)
             {
-                max1=max1+nums[i];
-            } else
-            {
-                max1=nums[i];
+                sum=0;
             }
-            if(max1>max2) max2=max1;
         }
-        return max2;
+        return max;
     }
 }
