bc9c4d822ab4c8f2189ae9bd173ff9f1c3431902
faran25 <mohd.faran25@gmail.com>
2024-05-28 23:58:52 +0000

    [LeetCode Sync] Runtime - 4 ms (93.06%), Memory - 41.6 MB (75.64%)

---
+++ my-folder/0518-coin-change-ii/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int dp[]=new int[amount+1];
+        dp[0]=1;
+        for(int i=0;i<coins.length;i++)
+        {
+            for(int j=0;j<=amount;j++)
+            {
+                if(coins[i]<=j)
+                {
+                    dp[j]=dp[j]+dp[j-coins[i]];
+                }
+            }
+        }
+        return dp[amount];
+    }
+}
