ea9c9f26bd0796a7483be54aae4e4d5b51a5c910
faran25 <mohd.faran25@gmail.com>
2024-06-10 20:36:31 +0000

    [LeetCode Sync] Runtime - 3 ms (35.07%), Memory - 46.6 MB (81.78%)

--- my-folder/0338-counting-bits/solution.java
+++ my-folder/0338-counting-bits/solution.java
@@ -1,28 +1,21 @@
 class Solution {
-    /*private int func(int n, int[] dp)
+    private int func(int n, int[] dp)
     {
-        if(n==0) return 0;
-        if(dp[n]>0) return dp[n];
+        if(dp[n]!=-1) return dp[n];
 
-        if(n%2==1) dp[n]=1+func(n/2,dp);
+        if(n%2==0) dp[n]=func(n/2,dp);
         else
-        dp[n]=func(n/2,dp);
+        dp[n]=1+func(n/2,dp);
         return dp[n];
-    }*/
+    }
     public int[] countBits(int n) {
         int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
         dp[0]=0;
         if(n==0) return dp;
-        dp[1]=1;
-        if(n==1) return dp;
-        ///dp[n]=func(n,dp);
-
-        for(int i=2;i<=n;i++)
-        {
-            if((i&1)==0) 
-                dp[i]=dp[i/2];
-            else 
-                dp[i]=1+dp[i/2];
+        //dp[n]=func(n,dp);
+        for(int i = 1; i <= n; i++) {
+            func(i, dp);
         }
         return dp;
     }
