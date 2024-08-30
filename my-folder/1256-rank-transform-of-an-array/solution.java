4827b0e577cebea45708a77d774954f83639e50e
faran25 <mohd.faran25@gmail.com>
2024-08-30 21:38:17 +0000

    [LeetCode Sync] Runtime - 20 ms (99.21%), Memory - 61.9 MB (69.82%)

---
+++ my-folder/1256-rank-transform-of-an-array/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int []ans=new int[arr.length];
+        for(int i=0;i<arr.length;i++)
+        {
+            ans[i]=arr[i];
+        }
+
+        Arrays.sort(arr);
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int count=1;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(!map.containsKey(arr[i]))
+            {
+                map.put(arr[i],count);
+                count++;
+            }
+        }
+        for(int i=0;i<ans.length;i++)
+        {
+           ans[i]=map.get(ans[i]);
+        }
+        return ans;
+    }
+}
