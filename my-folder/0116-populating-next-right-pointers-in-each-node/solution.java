e2d10fcc69cf4165d61178771ad3348236b68198
faran25 <mohd.faran25@gmail.com>
2024-09-22 09:56:20 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.5 MB (44.98%)

---
+++ my-folder/0116-populating-next-right-pointers-in-each-node/solution.java
@@ -0,0 +1,63 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    /*private void level(Node root, HashMap<Integer,Node> map, int level)
+    {
+        if(root==null) return;
+        if(map.containsKey(level)){
+        Node node=map.get(level);
+        node.next=root;
+        }
+        map.put(level,root);
+        level(root.left,map,level+1);
+        level(root.right,map,level+1);
+    }
+
+    public Node connect(Node root) {
+        HashMap<Integer,Node> map = new HashMap<>();
+        level(root,map,0);
+        return root;
+    }*/
+    private void level(Node root)
+    {
+        if(root==null) return;
+        if(root.left==null)
+        {
+            return;
+        }
+        //if(root.right!=null)
+        {
+            root.left.next=root.right;
+        }
+        if (root.next != null) {
+            root.right.next = root.next.left;
+        }
+        level(root.left);
+        level(root.right);
+    }
+    public Node connect(Node root) {
+        level(root);
+        return root;
+    }
+}
