37002446773d781b3b8a17f1f94a1175019e13f6
faran25 <mohd.faran25@gmail.com>
2024-12-30 22:06:11 +0000

    [LeetCode Sync] Runtime - 6 ms (32.76%), Memory - 44.9 MB (89.06%)

--- my-folder/0235-lowest-common-ancestor-of-a-binary-search-tree/solution.java
+++ my-folder/0235-lowest-common-ancestor-of-a-binary-search-tree/solution.java
@@ -9,11 +9,15 @@
  */
 
 class Solution {
+    //using
     public TreeNode lca(TreeNode root, int p, int q)
     {
         if(root==null) return null;
         int val=root.val;
-        if(val>p && val>q)
+        // if curr val is greater than both the values then answer lies in the left part of root
+        // if curr val is smaller than both the values then answer lies in the right part of root
+        //otherwise root is the answer
+        if(val>p && val>q) 
         {
             return lca(root.left, p, q);
         }else if(val<p && val<q)
@@ -25,4 +29,5 @@ class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         return lca(root, p.val, q.val);
     }
+    
 }
