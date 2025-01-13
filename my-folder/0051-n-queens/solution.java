53ec5c046c29d640007577d70f207340143c5eb1
faran25 <mohd.faran25@gmail.com>
2025-01-13 20:08:19 +0000

    [LeetCode Sync] Runtime - 5 ms (34.88%), Memory - 44.9 MB (55.38%)

---
+++ my-folder/0051-n-queens/solution.java
@@ -0,0 +1,69 @@
+class Solution {
+
+    HashSet<Integer> col=new HashSet<>();
+    HashSet<Integer> d1=new HashSet<>();
+    HashSet<Integer> d2=new HashSet<>();
+    public void convert(List<List<String>> ans, int[][] mat)
+    {
+        List<String> list=new ArrayList<>();
+        for(int i=0;i<mat.length;i++)
+        {
+            StringBuilder sb=new StringBuilder();
+            for(int j=0;j<mat.length;j++)
+            {
+                if(mat[i][j]==0) sb.append(".");
+                else sb.append("Q");
+            }
+            list.add(sb.toString());
+        }
+        ans.add(new ArrayList<>(list));
+        
+        /*System.out.println("----");
+        for(int i=0;i<mat.length;i++)
+        {
+            System.out.println();
+            for(int j=0;j<mat.length;j++)
+            {
+                System.out.print(mat[i][j]+" ");
+            }
+        }*/
+    }
+    
+    public void backtrack(List<List<String>> ans , int n, int[][] mat, int i)
+    {
+        if(n==0)
+        {
+            convert(ans,mat);
+            return;
+        }
+            i=col.size();
+            for(int j=0;j<mat.length;j++)
+            {
+                if(col.contains(j) || d1.contains(i-j) || d2.contains(i+j)) continue;
+                col.add(j);
+                d1.add(i-j);
+                d2.add(i+j);
+                mat[i][j]=1;
+                backtrack(ans,n-1,mat,i+1);
+                mat[i][j]=0;
+                col.remove(j);
+                d1.remove(i-j);
+                d2.remove(i+j);
+            }
+    }
+    public List<List<String>> solveNQueens(int n) {
+
+        int[][] mat=new int [n][n];
+        List<List<String>> ans=new ArrayList<>();
+        if(n==2 || n==3) return ans;
+        backtrack(ans,n,mat,0);
+        return ans;
+    }
+}
+/*
+0 1 0 0 0
+0 0 0 0 0
+0 0 0 0 0
+0 0 0 0 0
+0 0 0 0 0
+*/
