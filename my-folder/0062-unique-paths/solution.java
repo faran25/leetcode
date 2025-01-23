62fac2e25cd031e94b705d29e36c8df81b2bdefe
faran25 <mohd.faran25@gmail.com>
2025-01-23 14:46:31 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 39.8 MB (99.45%)

--- my-folder/0062-unique-paths/solution.java
+++ my-folder/0062-unique-paths/solution.java
@@ -2,14 +2,14 @@ class Solution {
     int [][] dp;
     public int backtrack(int m, int n)
     {
-        if(m==1 || n==1) return 1;
+        if(m==0 || n==0) return 1;
         if(dp[m][n]>0) return dp[m][n];
         dp[m][n]=backtrack(m-1,n)+backtrack(m,n-1);
         return dp[m][n];
     }
     public int uniquePaths(int m, int n) {
         if(m==1 || n==1) return 1;
-        dp=new int[m+1][n+1];
-        return backtrack(m,n);
+        dp=new int[m][n];
+        return backtrack(m-1,n-1);
     }
 }
